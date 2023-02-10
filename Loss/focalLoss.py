961a0c9f3049a0effb608af5ac5b8b5dd62669f9
Ahn-Ssu <ahn.ssu@kaist.ac.kr>
2023-02-10 14:47:29 +0900

    added Focal loss impl

---
+++ Loss/focalLoss.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+    
+        ce_loss = nn.CrossEntropyLoss()(inputs, targets, reduction='none')
+
+        pt = torch.exp(-ce_loss)  # CE(pt) = -log(pt) --> -ce_loss = log(pt) --> exp(log(pt)) --> pt
+        F_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
